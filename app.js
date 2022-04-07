38c1322e8babee8faeae483fdea42da592941db7
Luís Tovar <72473883+LuisTovar0@users.noreply.github.com>
2022-04-07 13:19:17 +0100

    Second release (regular JS)

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import del from 'del';
 import fs from "fs";
 
-export default async function newCleanFolder(folderName: string) {
+export default async function newCleanFolder(folderName) {
   await del(folderName);
   fs.access(folderName, (error) => {
     if (error) fs.mkdir(folderName, (error) => {
