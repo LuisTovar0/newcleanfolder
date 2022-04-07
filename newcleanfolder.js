dc216c68a4aa0a32dc0d9086e40cf160669ab498
Luís Tovar <72473883+LuisTovar0@users.noreply.github.com>
2022-04-07 13:47:45 +0100

    Another fix

--- newcleanfolder.js
+++ newcleanfolder.js
@@ -1,11 +1,11 @@
 const del = require('del');
 const fs = require('fs');
 
-export default async function newCleanFolder(folderName) {
+module.exports = async function newCleanFolder(folderName) {
   await del(folderName);
   fs.access(folderName, (error) => {
     if (error) fs.mkdir(folderName, (error) => {
       if (error) throw error;
     });
   });
-}
+};
