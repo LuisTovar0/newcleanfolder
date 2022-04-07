54c593cfe878b7a7300d4cf2d9cb7528bf25631e
Luís Tovar <72473883+LuisTovar0@users.noreply.github.com>
2022-04-07 20:04:42 +0100

    minfix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const del = require('del');
 const fs = require('fs');
 
-module.exports.newCleanFolder = async (folderName, silent) => {
+module.exports.newCleanFolder = async (folderName) => {
   await del(folderName);
   fs.access(folderName, (error) => {
     if (error) fs.mkdir(folderName, (error) => {
